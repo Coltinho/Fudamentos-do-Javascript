a9d537f20c2b279d25818af47ceda05876ed2748
Italo Colt <103470585+Coltinho@users.noreply.github.com>
2022-07-18 13:12:45 -0300

    Add files via upload

---
+++ javascript/parâmetros-argumentos.js
@@ -0,0 +1,9 @@
+function soma(numero1, numero2) {
+    return numero1 + numero2
+}
+
+function multi(numero1, numero2){
+    return numero1 * numero2
+}
+
+console.log(multi (soma(1 , 2) , soma(3,5)))
